35fca8325a62c902dad08343e52d9f51d98d90c1
Matt Dumford <mdumford99@gmail.com>
2019-05-25 21:52:52 -0500

    better navbar

--- www/js/index.js
+++ www/js/index.js
@@ -6,7 +6,7 @@ class LikeButton extends React.Component{
         this.state = {liked: false, hovered: false};
     }
 
-    clicked = () => this.setState({liked: !this.state.liked});
+    clicked = () => this.setState((state, props) => ({liked: !state.liked}));
 
     hovered = () => this.setState({hovered: true})
 
@@ -28,23 +28,49 @@ class LikeButton extends React.Component{
 class MouseTracker extends React.Component{
     constructor(props){
         super(props);
-        this.state = {xPos: 0, yPos: 0};
+        this.state = {xPos: 0, yPos: 0, enabled: false, hovered: false};
         this.handleMouseMove = this.handleMouseMove.bind(this);
+        this.toggle = this.toggle.bind(this);
+        this.hovered = this.hovered.bind(this);
+        this.notHovered = this.notHovered.bind(this);
     }
 
     toString(){return "<MouseTracker>";}
 
-    render(){
-        return <span className="badge badge-light MouseTracker">x={this.state.xPos}, y={this.state.yPos}</span>;
+    toggle(){
+        this.setState((state, props) => {
+            state.enabled = !state.enabled
+            if(this.state.enabled){
+                window.addEventListener('mousemove', this.handleMouseMove);
+            }
+            else{
+                window.removeEventListener('mousemove', this.handleMouseMove);
+            }
+            return {enabled: state.enabled}
+        });
+    }
+
+    hovered(){
+        this.setState({hovered: true});
+    }
+
+    notHovered(){
+        this.setState({hovered: false});
     }
 
     handleMouseMove(e){
         this.setState({xPos: e.pageX, yPos: e.pageY});
     }
 
+    render(){
+//        return <span className="badge badge-light MouseTracker">x={this.state.xPos}, y={this.state.yPos}</span>;
+        return <button type="button" data-toggle="button" className="btn btn-sm btn-outline-light" id="mouse-trigger-toggle" onMouseOver={this.hovered} onMouseOut={this.notHovered} onClick={this.toggle}>
+            {this.state.enabled && !this.state.hovered ? "x=" + this.state.xPos + ", " + "y=" + this.state.yPos : "Show/Hide"}
+        </button>;
+    }
+
     componentDidMount(){
-        console.log("Binding mouse tracker.");
-        window.addEventListener('mousemove', this.handleMouseMove);
+
     }
 
     componentWillUnmount(){
@@ -56,20 +82,18 @@ class MouseTracker extends React.Component{
 class Header extends React.Component{
     constructor(props){
         super(props);
-        this.state = {showMouseTracker: props.showMouseTracker};
-     }
-
-     toggleMouseTracker(e){
-        this.setState({showMouseTracker: !this.state.showMouseTracker});
      }
 
     render(){
         return(
-            <div id="header">
-                <LikeButton />
-                <button type="button" data-toggle="button" className="btn btn-sm btn-outline-secondary" id="mouse-trigger-toggle" onClick={(e) => this.toggleMouseTracker(e)}>Show/Hide</button>
-                {this.state.showMouseTracker && <MouseTracker />}
-            </div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-1" id="header">
+                <ul className="navbar-nav">
+                    <li className="nav-item"><a href="#" className="navbar-brand">Serv</a></li>
+                </ul>
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item"><MouseTracker /></li>
+                </ul>
+            </nav>
         );
     }
 }
@@ -81,8 +105,9 @@ class App extends React.Component{
 
     render(){
         return(
-            <div className="container-fluid">
-                <Header showMouseTracker={false}/>
+            <div className="container-fluid px-0">
+                <Header />
+                <LikeButton />
             </div>
         );
     }
